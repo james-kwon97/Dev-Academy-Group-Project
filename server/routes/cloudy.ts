876d239411abfefd4f485a2aea885b3587cfafb0
Gabriel Rawcliffe <gabriel.rawcliffe@gmail.com>
2023-10-04 14:14:37 +1300

    minor edit to routes

--- server/routes/cloudy.ts
+++ server/routes/cloudy.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-import { Welcome } from '../../models/welcome.ts'
 import request from 'superagent'
 
 import 'dotenv/config'
@@ -8,17 +7,17 @@ const router = express.Router()
 
 // GET /api/v1/cloudy/
 
-router.get('/', (req, res) => {
-  try {
-    res.json({ statement: 'Welcome to external APIs!' } as Welcome)
-  } catch (err) {
-    if (err instanceof Error) {
-      res.status(500).send((err as Error).message)
-    } else {
-      res.status(500).send('Something went wrong')
-    }
-  }
-})
+// router.get('/', (req, res) => {
+//   try {
+//     res.json({ statement: 'Welcome to external APIs!' } as Welcome)
+//   } catch (err) {
+//     if (err instanceof Error) {
+//       res.status(500).send((err as Error).message)
+//     } else {
+//       res.status(500).send('Something went wrong')
+//     }
+//   }
+// })
 
 // Get route for weather API
 
