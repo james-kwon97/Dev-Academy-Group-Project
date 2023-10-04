d7d3e14cf21c754dd087833e029725ce054b630a
Gabriel Rawcliffe <gabriel.rawcliffe@gmail.com>
2023-10-04 14:02:32 +1300

    dokku setup

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,10 @@
 import server from './server.ts'
+import dotenv from 'dotenv'
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  const envConfig = dotenv.config()
+  if (envConfig.error) throw envConfig.error
+}
 
 const port = process.env.PORT || 3000
 
